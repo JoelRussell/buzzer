ac68d7a1cbbe9af1fd0cee23f1a90c7bf88c8229
Joel <joel@joel.pm>
2025-01-27 19:16:34 +0000

    Dynamic sound

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+// read all filenames from sounds folder and store in sounds array
+const fs = require('fs')
+const sounds = fs.readdirSync('./public/sounds').map(sound => sound.split('.')[0])
+
+console.log(sounds)
+
 const title = 'Buffer Buzzer'
 
 let data = {
@@ -13,15 +19,18 @@ let data = {
   buzzes: new Set(),
 }
 
+
 const getData = () => ({
   users: [...data.users],
-  buzzes: [...data.buzzes]
+  buzzes: [...data.buzzes],
+  sounds
 })
+
 
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.render('index', { title }))
+app.get('/', (req, res) => res.render('index', { title, sounds } ))
 app.get('/host', (req, res) => res.render('host', Object.assign({ title }, getData())))
 
 io.on('connection', (socket) => {
